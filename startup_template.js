cf3077cdb04a723c92c71a11420aec07f310c06c
galazg <ffer17@gmail.com>
2016-11-18 16:12:06 +0100

    updated startup template

--- startup_template.js
+++ startup_template.js
@@ -1,8 +1,8 @@
-var fer = personal.listAccounts[0]
-var jon = personal.listAccounts[1]
-var mike = personal.listAccounts[2]
+var fer = "0x9ec2f830dae76d1b5d4d3e32daf1d96a4e51d05d"
+var jon = "0x3beb7c45f3ae37930982841c34f3098ea6e2360f"
+var mike = "0x1b69f21849d0942fab15de236def687b05d29472"
 
-var pCoinAdd = ""
+var pCoinAdd = "0x54967a8db5b8bdf8600eea533a06f3172d026e56"
 var interface_ = [{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendTokens","outputs":[{"name":"successful","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"user","type":"address"}],"name":"validate","outputs":[{"name":"level","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balances","outputs":[{"name":"tokens","type":"uint256"},{"name":"rightsLevel","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"user","type":"address"}],"name":"reset","outputs":[{"name":"successful","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"payingUser","type":"address"},{"name":"rightsLevelReq","type":"uint256"}],"name":"register","outputs":[{"name":"flag","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"payingUser","type":"address"},{"name":"rightsLevelReq","type":"uint256"}],"name":"checkMoney","outputs":[{"name":"flag","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[],"type":"constructor"}]
 var pcoin = eth.contract(interface_).at(pCoinAdd);
 
