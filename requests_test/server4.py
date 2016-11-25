6f8645208c60feec969de07e546430f73ea5fe50
galazg <ffer17@gmail.com>
2016-11-25 17:31:56 +0100

    25 nov 16

--- requests_test/server4.py
+++ requests_test/server4.py
@@ -5,7 +5,7 @@ import SocketServer
 from BaseHTTPServer import BaseHTTPRequestHandler
 import requests
 from os import curdir, sep
-
+import urlparse, json
 
 pcoin_contract_address = '0xf03f59fa47ec6680a3b6d84c7a62471553b72840'
 fer =  '0x14c1b2ed09229c2df7c04ec92115ece6d1eabe73'
@@ -24,25 +24,25 @@ def getAddress(user):
         return '0x0000000000000000000000000000000000000000';
 
 class MyHandler(BaseHTTPRequestHandler):
-    def do_GET(s):
+    def do_GET(self):
 
-        if s.path =='/':
+        if self.path =='/':
         	#Open the static file requested and send it
-            s.path = '/test.html'
+            self.path = '/testjson.html'
             mimetype='text/html'
-            f = open(curdir + sep + s.path)
-            print curdir + sep + s.path
-            s.send_response(200)
-            s.send_header('Content-type',mimetype)
-            s.end_headers()
-            s.wfile.write(f.read())
+            f = open(curdir + sep + self.path)
+            print curdir + sep + self.path
+            self.send_response(200)
+            self.send_header('Content-type',mimetype)
+            self.end_headers()
+            self.wfile.write(f.read())
             f.close()
 
-#        if s.path == '/validate':
-        if s.path.find('validate') > 0:
+#        if self.path == '/validate':
+        if self.path.find('validate') > 0:
             print('Function called: validate')
-            p = s.path.find('&')
-            user = s.path[p+1:len(s.path)]
+            p = self.path.find('&')
+            user = self.path[p+1:len(self.path)]
             user_address = getAddress(user)
             #this parameter is only for the "validate" method of the contract
             rpc_data = '0x207c64fb000000000000000000000000' + user_address[2:len(user_address)]
@@ -51,25 +51,25 @@ class MyHandler(BaseHTTPRequestHandler):
             #this only works when the "validate" method was called
             print('RightsLevel: ' + r.text[len(r.text)-4])
             print r.text
-            print s.path
-            s.send_response(200)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            s.wfile.write(r.text)
-            #s.wfile.write(user + " has rights level " + result)
+            print self.path
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(r.text)
+            #self.wfile.write(user + " has rights level " + result)
 
-            #s.wfile.write("<html><head><title>Private blockchain</title></head>")
-            #s.wfile.write("<body><p>Validate user.</p>")
-            #s.wfile.write("<p>User: %s</p>" % user)
-            #s.wfile.write("<p>Rights level: %s</p>" % result)
-            #s.wfile.write("</body></html>")
+            #self.wfile.write("<html><head><title>Private blockchain</title></head>")
+            #self.wfile.write("<body><p>Validate user.</p>")
+            #self.wfile.write("<p>User: %s</p>" % user)
+            #self.wfile.write("<p>Rights level: %s</p>" % result)
+            #self.wfile.write("</body></html>")
 
-        elif s.path.find('register') > 0:
+        elif self.path.find('register') > 0:
             print('Function called: register')
-            p = s.path.find('&')
-            q = s.path.find('=')
-            user = s.path[p+1:q]
-            level_requested = s.path[q+1:len(s.path)]
+            p = self.path.find('&')
+            q = self.path.find('=')
+            user = self.path[p+1:q]
+            level_requested = self.path[q+1:len(self.path)]
             print(user)
             print(level_requested)
             user_address = getAddress(user)
@@ -83,20 +83,20 @@ class MyHandler(BaseHTTPRequestHandler):
             #get the rightsLevel from the response
             result = r.text[len(r.text)-4]
             print result
-            print s.path
-            s.send_response(200)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            #s.wfile.write("<html><head><title>Private blockchain</title></head>")
-            #s.wfile.write("<p>User: %s</p>" % user)
-            #s.wfile.write("<p>Rights level requested: %s</p>" % level_requested)
+            print self.path
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            #self.wfile.write("<html><head><title>Private blockchain</title></head>")
+            #self.wfile.write("<p>User: %s</p>" % user)
+            #self.wfile.write("<p>Rights level requested: %s</p>" % level_requested)
 
 
             #if the user is already registered
             if int(result) > 0:
-                s.wfile.write('{"result":"already registered"}')
+                self.wfile.write('{"result":"already registered"}')
                 #print('User already registered with rights level: ' + result)
-                #s.wfile.write("<p>User was already registered with rights level: %s</p>" % result)
+                #self.wfile.write("<p>User was already registered with rights level: %s</p>" % result)
             else:
                 #check if there is enough money, and if so, register the user.
                 #build data parameter for "checkMoney" RPC call
@@ -107,38 +107,38 @@ class MyHandler(BaseHTTPRequestHandler):
 
                 #if enough money, register
                 if int(result) == 1:
-                    s.wfile.write('{"result":"register successful"}')
-                    #s.wfile.write("<p>The user has enough funds.</p>")
-                    #s.wfile.write("<p>The user has been registered and the tokens deducted.</p>")
-                    #s.wfile.write("</body></html>")
+                    self.wfile.write('{"result":"register successful"}')
+                    #self.wfile.write("<p>The user has enough funds.</p>")
+                    #self.wfile.write("<p>The user has been registered and the tokens deducted.</p>")
+                    #self.wfile.write("</body></html>")
                     #send the register transaction!
                     rpc_data = '0x6d705ebb000000000000000000000000' + user_address[2:len(user_address)] + '000000000000000000000000000000000000000000000000000000000000000' + level_requested
                     data = '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"' + sender + '", "to":"' + pcoin_contract_address + '", "data": "' + rpc_data +'"}],"id":1}'
                     r = requests.post('http://localhost:8001', data=data)
                     print r.text
                 else:
-                    s.wfile.write('{"result":"no money"}')
+                    self.wfile.write('{"result":"no money"}')
                     print('User does NOT exist or does NOT have enough funds.')
-                    #s.wfile.write("<p>User does NOT exist or does NOT have enough funds.</p>")
-                    #s.wfile.write("</body></html>")
+                    #self.wfile.write("<p>User does NOT exist or does NOT have enough funds.</p>")
+                    #self.wfile.write("</body></html>")
 
 
-        elif s.path.find('miner_start') > 0:
+        elif self.path.find('miner_start') > 0:
             print ('Start mining!')
             data = '{"jsonrpc":"2.0","method":"miner_start","params":[],"id":74}'
             r = requests.post('http://localhost:8001', data=data)
             print r.text
-        elif s.path.find('miner_stop') > 0:
+        elif self.path.find('miner_stop') > 0:
             print('Stop mining!')
-        elif s.path.find('is_mining') > 0:
+        elif self.path.find('is_mining') > 0:
             data = '{"jsonrpc":"2.0","method":"eth_mining","params":[],"id":71}'
             r = requests.post('http://localhost:8001', data=data)
             print r.text
 
-        elif s.path.find('reset') > 0:
+        elif self.path.find('reset') > 0:
             print('Function called: reset')
-            p = s.path.find('&')
-            user = s.path[p+1:len(s.path)]
+            p = self.path.find('&')
+            user = self.path[p+1:len(self.path)]
             print(user)
             user_address = getAddress(user)
             #this parameter is only for the "reset" method of the contract
@@ -147,24 +147,24 @@ class MyHandler(BaseHTTPRequestHandler):
             r = requests.post('http://localhost:8001', data=data)
             print('Balances have been cleared.')
             print r.text
-            print s.path
-            s.send_response(200)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            s.wfile.write(r.text)
-            #s.wfile.write("<html><head><title>Private blockchain</title></head>")
-            #s.wfile.write("<body><p>The balances have been cleared.</p>")
-            #s.wfile.write("<p>User: %s</p>" % user)
-            #s.wfile.write("</body></html>")
+            print self.path
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(r.text)
+            #self.wfile.write("<html><head><title>Private blockchain</title></head>")
+            #self.wfile.write("<body><p>The balances have been cleared.</p>")
+            #self.wfile.write("<p>User: %s</p>" % user)
+            #self.wfile.write("</body></html>")
 
-        elif s.path.find('topup') > 0:
+        elif self.path.find('topup') > 0:
             print('Function called: sendTokens')
-            p = s.path.find('&')
-            q = s.path.find('=')
-            user = s.path[p+1:q]
+            p = self.path.find('&')
+            q = self.path.find('=')
+            user = self.path[p+1:q]
             print(user)
             user_address = getAddress(user)
-            amount = s.path[q+1:len(s.path)]
+            amount = self.path[q+1:len(self.path)]
             amount_hex = str(hex(int(amount)))
             amount_hex_trimmed = amount_hex[amount_hex.find('x')+1:len(amount_hex)]
             amount_hex_padded = '0000000000000000000000000000000000000000000000000000000000000000'
@@ -174,16 +174,33 @@ class MyHandler(BaseHTTPRequestHandler):
             data = '{"jsonrpc":"2.0","method":"eth_sendTransaction","params":[{"from":"' + sender + '", "to":"' + pcoin_contract_address + '", "data": "' + rpc_data +'"}],"id":1}'
             r = requests.post('http://localhost:8001', data=data)
             print('Top up with tokens')
-            print s.path
-            s.send_response(200)
-            s.send_header("Content-type", "text/html")
-            s.end_headers()
-            #s.wfile.write("<html><head><title>Private blockchain</title></head>")
-            #s.wfile.write("<body><p>Top up with tokens.</p>")
-            #s.wfile.write("<p>User: %s</p>" % user)
-            #s.wfile.write("<p>Amount loaded: %s</p>" % amount)
-            #s.wfile.write("</body></html>")
-            s.wfile.write(r.text)
+            print self.path
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            #self.wfile.write("<html><head><title>Private blockchain</title></head>")
+            #self.wfile.write("<body><p>Top up with tokens.</p>")
+            #self.wfile.write("<p>User: %s</p>" % user)
+            #self.wfile.write("<p>Amount loaded: %s</p>" % amount)
+            #self.wfile.write("</body></html>")
+            self.wfile.write(r.text)
+        return
+
+    def do_POST(self):
+        content_len = int(self.headers.getheader('content-length'))
+        post_body = self.rfile.read(content_len)
+        self.send_response(200)
+        self.end_headers()
+        data = json.loads(post_body)
+        # data is the json object received from the web ui
+        print data
+        # this is how the elements of the json object are used
+        print data["command"]
+        # need to construct the json object that will be sent to the geth node, with the parameters received from web ui
+
+        #an example of the JSON format to be sent
+        self.wfile.write('{"jsonrpc":"2.0","method":"validate"}')
+        return
 
 httpd = SocketServer.TCPServer(("", 8084), MyHandler)
 httpd.serve_forever()
